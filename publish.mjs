3cd19acfc0fcd4b60bd46801554e09a2b2232520
hoge1e3 <hoge1e3@gmail.com>
2025-03-20 12:03:41 +0900

    oops

--- publish.mjs
+++ publish.mjs
@@ -3,7 +3,7 @@ const sh = await create();
 const pkg = sh.rel("./package.json").obj();
 console.log(pkg.workspaces);
 const otp = process.argv[2];
-if (!opt) throw new Error("Please specify otp from Authenticater App.");
+if (!otp) throw new Error("Please specify otp from Authenticater App.");
 for (let workspace of pkg.workspaces) {
   console.log(workspace); 
   //sh.pushd(workspace);
