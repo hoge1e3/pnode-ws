a08464ff583072061c980d57c8c060f15288a662
hoge1e3 <hoge1e3@gmail.com>
2025-03-24 17:18:54 +0900

    fix clone / add sync-idb-kvs-multi

--- clone.mjs
+++ clone.mjs
@@ -1,10 +1,10 @@
-import {Shell} from './shell.mjs';
-const sh = new Shell();
+import {create} from './shell.mjs';
+const sh = await create();
 const pkg = sh.rel("./package.json").obj();
 console.log(pkg.workspaces);
 for (let workspace of pkg.workspaces) {
   console.log(workspace); 
   sh.pushd(workspace);
-  await exec("git", ["clone", `git@github.com:hoge1e3/${workspace}.git`]);
+  await sh.exec("git", ["clone", `git@github.com:hoge1e3/${workspace}.git`]);
   sh.popd();
 }
