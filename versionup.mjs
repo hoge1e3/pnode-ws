7448de03afc3c9c2f7673610964f164fc7b0c6ac
hoge1e3 <hoge1e3@gmail.com>
2025-03-25 15:31:08 +0900

    Build strategy change / tsc -> webpack  , 1.3.0

--- versionup.mjs
+++ versionup.mjs
@@ -2,7 +2,7 @@ import {create} from './shell.mjs';
 const sh = await create();
 const pkg = sh.rel("./package.json").obj();
 console.log(pkg.workspaces);
-const ver="1.2.9";
+const ver="1.3.0";
 for (let workspace of pkg.workspaces) {
   console.log(workspace); 
   sh.pushd(workspace);
