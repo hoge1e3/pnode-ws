1a22ba02fb4d9c8eec194bf1a812c763e45e2593
hoge1e3 <hoge1e3@gmail.com>
2025-03-26 11:51:48 +0900

    1.3.1(fail)

--- versionup.mjs
+++ versionup.mjs
@@ -2,7 +2,7 @@ import {create} from './shell.mjs';
 const sh = await create();
 const pkg = sh.rel("./package.json").obj();
 console.log(pkg.workspaces);
-const ver="1.3.0";
+const ver="1.3.1";
 for (let workspace of pkg.workspaces) {
   console.log(workspace); 
   sh.pushd(workspace);
