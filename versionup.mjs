c89613c92ba705cc89f56a7c3e4f771e271058ac
hoge1e3 <hoge1e3@gmail.com>
2025-07-15 14:15:58 +0900

    1.4.2

--- versionup.mjs
+++ versionup.mjs
@@ -14,7 +14,7 @@ for (let workspace of pkg.workspaces) {
   sh.popd();
 }
 const pnode=sh.rel("petit-node");
-const files=[pnode.rel("js/index.js"),pnode.rel("dist/index.js")];
+const files=[pnode.rel("src/index.ts"),pnode.rel("js/index.js"),pnode.rel("dist/index.js")];
 for (let file of files) {
   file.text(file.text().replace(/__VER__[\d\.]+__SION__/, `__VER__${ver}__SION__`));
 }
