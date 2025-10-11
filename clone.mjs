e6111b15678d1aed33fc8386c71a9377283ddd3a
hoge1e3 <hoge1e3@gmail.com>
2025-10-11 16:41:35 +0900

    ssh clone?

--- clone.mjs
+++ clone.mjs
@@ -1,15 +1,13 @@
 
 import * as fs from "fs";
-import * as process from 'process';
 import * as cp from 'child_process';
-//const sh = await create();
-// read package.json of current dir
+const user="hoge1e3";
+const ssh=true;
+const tourl=(name)=>ssh?`git@github.com:${user}/${name}.git`:`https://github.com/${user}/${name}.git`
 const pkg =  JSON.parse(fs.readFileSync('package.json', 'utf8'));
-let me=process.argv.includes("-me");//Options just for me, @hoge1e3 :-)
 console.log(pkg.workspaces);
 for (let workspace of pkg.workspaces) {
   console.log(workspace); 
-  const url=me?`git@github.com:hoge1e3/${workspace}.git`:`https://github.com/hoge1e3/${workspace}.git`
-  // run git clone $url
+  const url=tourl(workspace);
   cp.execSync(`git clone ${url}`, {stdio: 'inherit'});
 }
--- clone_acepad.mjs
+++ clone_acepad.mjs
@@ -2,8 +2,9 @@
 import * as fs from "fs";
 import * as process from 'process';
 import * as cp from 'child_process';
-let me=process.argv.includes("-me");//Options just for me, @hoge1e3 :-)
-const tourl=(name)=>me?`git@github.com:hoge1e3/${name}.git`:`https://github.com/hoge1e3/${name}.git`
+const user="hoge1e3";
+const ssh=true;
+const tourl=(name)=>ssh?`git@github.com:${user}/${name}.git`:`https://github.com/${user}/${name}.git`
 for (let workspace of ["pnode-bootkit"]) {
   console.log(workspace); 
   const url=tourl(workspace);
