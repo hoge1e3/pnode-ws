3b320594a966df62dbfe9d4aed329f8394eead25
hoge1e3 <hoge1e3@gmail.com>
2026-01-05 13:02:44 +0900

    gitsync: . is last

--- gitsync_all.mjs
+++ gitsync_all.mjs
@@ -2,7 +2,7 @@ import {create} from './shell.mjs';
 const sh = await create();
 const pkg = sh.rel("./package.json").obj();
 //console.log(pkg.workspaces);
-const paths=[".", ...pkg.workspaces];
+const paths=[ ...pkg.workspaces,"."];
 const unsyncs=[];
 for (let path of paths){
     if (await sh.exec("node",["gitsync.mjs",path],{nostdout:true})) { 
