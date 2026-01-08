887df273f95ef94b8564a49b8071b57cd08dceac
hoge1e3 <hoge1e3@gmail.com>
2026-01-08 12:25:19 +0900

    boot in worker (passed?)

--- bootkit-pack/js/worker.js
+++ bootkit-pack/js/worker.js
@@ -1,43 +1,43 @@
 import * as rpc from "@hoge1e3/rpc";
 import * as pNode from "petit-node";
 /*global self */
-export async function run({
+export async function boot({
   fstab,main,id
 }){
-try{
-  (function hackTimeouts(){
-    for(let k of ["setTimeout","setInterval",
-    "clearTimeout","clearInterval",]){
-        globalThis[k]=globalThis[k].bind(globalThis);
-    }
-  })();
-  await pNode.boot();
-  globalThis.pNode=pNode;
-  const dev=pNode.getDeviceManager();
-  await dev.loadFstab(fstab);
-  const FS=pNode.getFS();
-  //await FS.mountAsync("/idb/","idb");
-  if(process.env.POLICY_TOPDIR){
-    FS.setDefaultPolicy({topDir:process.env.POLICY_TOPDIR});  
-  }
-
-  Error.stackTraceLimit=100;
-  const mod=await pNode.importModule(FS.get(main));
-  const srv={};
-  const rev_cli=rpc.proxy.client(self, id);
-  for(let k in mod){
-      let v=mod[k];
-      if(typeof v==="function"){
-          srv[k]=v.bind(rev_cli);
+  try{
+    /*(function hackTimeouts(){
+      for(let k of ["setTimeout","setInterval",
+      "clearTimeout","clearInterval",]){
+          globalThis[k]=globalThis[k].bind(globalThis);
       }
-  }
-  rpc.proxy.server("default",[],srv);
-  console_client();
+    })();*/
+    await pNode.boot();
+    globalThis.pNode=pNode;
+    const dev=pNode.getDeviceManager();
+    await dev.loadFstab(fstab);
+    const FS=pNode.getFS();
+    //await FS.mountAsync("/idb/","idb");
+    if(process.env.POLICY_TOPDIR){
+      FS.setDefaultPolicy({topDir:process.env.POLICY_TOPDIR});  
+    }
 
-}catch(e) {
-  self.postMessage({stack:e.stack});
-  console.error(e);
-}
+    Error.stackTraceLimit=100;
+    const mod=await pNode.importModule(FS.get(main));
+    const srv={};
+    const rev_cli=rpc.proxy.client(self, id);
+    for(let k in mod){
+        let v=mod[k];
+        if(typeof v==="function"){
+            srv[k]=v.bind(rev_cli);
+        }
+    }
+    rpc.proxy.server("default",[],srv);
+    console_client();
+
+  }catch(e) {
+    self.postMessage({stack:e.stack});
+    console.error(e);
+  }
 }
 
 
@@ -65,6 +65,6 @@ export function console_client(){
     
     return c;
 }
-if(typeof self!==undefined){
-  
+export function startWorker(){
+  rpc.proxy.server("boot",[], {boot});
 }
